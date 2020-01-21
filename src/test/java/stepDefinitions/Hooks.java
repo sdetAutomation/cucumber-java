0388ff96087038d016b2aaf2f56c1102becc8651
hkw <hkw@hkws-MacBook-Pro.local>
2020-01-20 22:04:31 -0600

    chore: add hooks/after test code to hooks class

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
 public class Hooks {
@@ -7,5 +8,10 @@ public class Hooks {
     @Before("@MobileTest")
     public void beforeValidation(){
         System.out.println("Hooks-beforeValidation");
+    }
+
+    @After("@MobileTest")
+    public void afterValidation(){
+        System.out.println("Hooks-afterValidation");
     }
 }
